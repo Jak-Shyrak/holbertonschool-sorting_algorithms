548533527dc4daf5e0573bd0356aaee6b6f3fbce
Florent1711 <graphit01@hotmail.com>
2024-01-15 07:45:12 -0800

    modification 0-bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,24 +4,33 @@
  * @a: Integer a
  * @b: Integer b
  */
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 /**
  * bubble_sort - Doubly linked list node
  * @array: Array of integers to print
  * @size: Size of the array
  */
-void bubble_sort(int *array, size_t size) 
+void bubble_sort(int *array, size_t size)
 {
-    for (size_t i = 0; i < size - 1; i++) {
-        for (size_t j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                swap(&array[j], &array[j + 1]);
-                print_array(array, size);   
-            }
-        }
-    }
+	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
 }
