a5f68bdd42c25a5000d7bdf60be91b768e7752c2
Jak-Shyrak <bourhlef.yacine53@gmail.com>
2024-01-15 14:51:04 +0100

    Updating 0-bubble_sort.c (Added print function forgotten)

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -20,7 +20,9 @@ void bubble_sort(int *array, size_t size)
         for (size_t j = 0; j < size - i - 1; j++) {
             if (array[j] > array[j + 1]) {
                 swap(&array[j], &array[j + 1]);
+                print_array(array, size);   
             }
+            
         }
     }
 }
