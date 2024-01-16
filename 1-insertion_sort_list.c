55a92508bd45ab9733c13ec7881eab7f4b8ae01c
Florent1711 <graphit01@hotmail.com>
2024-01-16 03:53:48 -0800

    modifications et correction du fichier 1-insertion_sort_list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -5,36 +5,33 @@
  * ascending order
  * @list: List to be sorted
  */
+
 void insertion_sort_list(listint_t **list)
 {
-    if (list == NULL || *list == NULL)
-        return;
+	listint_t *current = (*list)->next;
+	listint_t *temp = current;
 
-    listint_t *current = (*list)->next;
+	if (list == NULL || *list == NULL)
+		return;
 
-    while (current != NULL)
-    {
-        listint_t *temp = current;
+	while (current != NULL)
+	{
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			temp->prev->next = temp->next;
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
 
-        while (temp->prev != NULL && temp->n < temp->prev->n)
-        {
-            temp->prev->next = temp->next;
-            if (temp->next != NULL)
-                temp->next->prev = temp->prev;
+			if (temp->prev != NULL)
+				temp->prev->next = temp;
 
-            temp->next = temp->prev;
-            temp->prev = temp->prev->prev;
-
-            if (temp->prev != NULL)
-                temp->prev->next = temp;
-
-            else
-                *list = temp;
-
-            temp->next->prev = temp;
-            print_list(*list);
-        }
-
-        current = current->next;
-    }
+			else
+				*list = temp;
+			temp->next->prev = temp;
+		}
+		current = current->next;
+		print_list(*list);
+	}
 }
